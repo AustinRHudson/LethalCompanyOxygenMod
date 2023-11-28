8e0a6eda4eba9837cb099171e0599d6998c50310
Austin R Hudson <hudsonaustin25@gmail.com>
2023-11-28 16:42:40 -0600

    Outline for the mod

--- LethalCompanyTemplate/Plugin.cs
+++ LethalCompanyTemplate/Plugin.cs
@@ -1,14 +1,27 @@
 ﻿using BepInEx;
+using HarmonyLib;
 
 namespace LethalCompanyTemplate
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class LethalCompanyTemplate : BaseUnityPlugin
     {
+        private const string modGUID = "TestModTThatIsRightMyGuy";
+        private const string modName = "Lethal Company Test Mod";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static LethalCompanyTemplate Instance;
         private void Awake()
         {
+            if (Instance == null) {
+                Instance = this;
+            }
             // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Logger.LogInfo($"Plugin {modGUID} is loaded!");
+
+            harmony.PatchAll(typeof(LethalCompanyTemplate));
         }
     }
 }
